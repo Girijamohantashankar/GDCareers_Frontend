962b171cb0dc8c52b21153d17f4fcf2bfd9c94dd
Harpreet Singh <harpreetsinghmehra116@gmail.com>
2023-06-06 15:20:40 +0530

    /

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  select:any=[];
+  filterz:any=[
+    {
+      id:0,
+      title:"Information Technology",
+      select:true
+    },
+    {
+      id:1,
+      title:"Healthcare",
+      select:false
+    },
+    {
+      id:2,
+      title:"Education & Technology",
+      select:false
+    },
+    {
+      id:3,
+      title:"Finance",
+      select:false
+    },
+  ];
   accountsettings:boolean=false;
   usersetting(){
    this.accountsettings=!this.accountsettings
+  }
+
+  onChange(i:any){
+      this.filterz[i].select=!this.filterz[i].select;
+      if(this.filterz[i].select==true)
+      {
+        this.filterz[i].push(i);
+      }
+      else{
+        this.select=this.select.filter((item:any)=>{
+            return item.id!=i;
+        })
+      }
   }
 
 
